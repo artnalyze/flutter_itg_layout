69ba2f91102694df0e09a3291e9538030b7563de
Artnalyze <artnalyze@gmail.com>
2020-06-25 23:30:56 +0700

    align widget

---
+++ lib/align_widget_layout.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class AlignWidgetPage extends StatefulWidget {
+  AlignWidgetPage({Key key}) : super(key: key);
+
+  @override
+  _AlignWidgetPageState createState() => _AlignWidgetPageState();
+}
+
+class _AlignWidgetPageState extends State<AlignWidgetPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Align widget Layout"),
+        backgroundColor: Colors.brown,
+      ),
+      body: Center(
+        child: Container(
+          width: 300,
+          height: 300,
+          color: Colors.teal,
+          child: Align(
+            alignment: Alignment.center,
+            child: Text(
+              "Hello",
+              style: TextStyle(color: Colors.white, fontSize: 40.0),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
